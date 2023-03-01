96872ccabf56b3343bb9bfb75f0137982c03d21e
Robin <loeveet@gmail.com>
2023-03-01 14:00:36 +0100

    Show muscles

---
+++ TrainingAppMauiVersion2/NewFolder/HelperMethods.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingAppMauiVersion2.NewFolder
+{
+    class HelperMethods
+    {
+        public static string[] CapitalizeFirstLetterInArray(string[] array)
+        {
+            string[] arrayForShow = new string[array.Length];
+            int i = 0;
+            foreach (var item in array)
+            {
+                char first = item[0];
+                arrayForShow[i] = char.ToUpper(item[0]) + item.Substring(1).Replace('_', ' ');
+                i++;
+            }
+            return arrayForShow;
+        }
+        public static double ConvertKgToPounds(double kg)
+        {
+            double pounds = kg * 2.2046226218;
+            return Math.Round(pounds);
+        }
+        public static int TryParseToInt(string input)
+        {
+            int id;
+            while (!int.TryParse(input, out id))
+            {
+                Console.WriteLine("Wrong input, try again");
+                input = Console.ReadLine();
+            }
+            return id;
+        }
+    }
+}
--- TrainingAppMauiVersion2/ViewModels/CreateTrainingProgramViewModel.cs
+++ TrainingAppMauiVersion2/ViewModels/CreateTrainingProgramViewModel.cs
@@ -6,8 +6,10 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using TrainingAppMauiVersion2.Models;
+using TrainingAppMauiVersion2.NewFolder;
 using TrainingAppMauiVersion2.SessionData;
 
 namespace TrainingAppMauiVersion2.ViewModels
@@ -22,11 +24,24 @@ namespace TrainingAppMauiVersion2.ViewModels
 
         [ObservableProperty]
         string name;
+
+        [ObservableProperty]
+        public ObservableCollection<string> muscles;
+
         public CreateTrainingProgramViewModel()
         {
             Person = SiteVariables.LoggedInPerson;
+            Muscles = new ObservableCollection<string>();
+            AddMuscles();      
         }
-
+        private void AddMuscles()
+        {
+            var muscleArray = HelperMethods.CapitalizeFirstLetterInArray(Exercise.muscles);
+            foreach (var m in muscleArray)
+            {
+                Muscles.Add(m);
+            }
+        }
 
         [RelayCommand]
         public async void ClickedGetProgramsButton()
@@ -56,6 +71,25 @@ namespace TrainingAppMauiVersion2.ViewModels
         private static async Task SaveProgram(TrainingProgram program, IMongoCollection<TrainingProgram> myCollection)
         {
             await myCollection.InsertOneAsync(program);
+        }
+
+        [RelayCommand]
+        public static async Task<ObservableCollection<Exercise>> GetExercices(string muscle)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://api.api-ninjas.com/");
+            client.DefaultRequestHeaders.Add("X-Api-Key", "E2O3R8zknVI8Lo/k0kdq7A==JBFTCWQdZStbgUQq");
+
+            ObservableCollection<Exercise> exercises = null;
+
+            HttpResponseMessage response = await client.GetAsync(muscle);
+            if (response.IsSuccessStatusCode)
+            {
+                string responseString = await response.Content.ReadAsStringAsync();
+                exercises = JsonSerializer.Deserialize<ObservableCollection<Exercise>>(responseString);
+            }
+
+            return exercises;
         }
     }
 }
