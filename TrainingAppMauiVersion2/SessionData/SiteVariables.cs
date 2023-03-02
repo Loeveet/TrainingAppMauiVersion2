6b04e06242374e76b354a0e74b2299193ee0a568
Robin <loeveet@gmail.com>
2023-03-02 23:22:48 +0100

    sitevariables

--- TrainingAppMauiVersion2/SessionData/SiteVariables.cs
+++ TrainingAppMauiVersion2/SessionData/SiteVariables.cs
@@ -9,7 +9,7 @@ namespace TrainingAppMauiVersion2.SessionData
     internal static class SiteVariables
     {
         public static string SearchOtherExercises { get; set; }
-        public static Models.Person LoggedInPerson { get; set; } // kanske inte behöver
+        public static Models.Person LoggedInPerson { get; set; } 
         public static string ChosenMuscle { get; set; }
         public static string ChosenDifficultness { get; set; } = string.Empty;
         public static string ChosenTypeOfExercise { get; set; } = string.Empty;
